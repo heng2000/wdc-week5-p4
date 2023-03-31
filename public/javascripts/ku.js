1458a8760231a65d3e89ff427ce3277ae49efb04
a <liyuhengduigong@gmail.com>
2023-03-31 20:37:53 +1030

    li changed

--- public/javascripts/ku.js
+++ public/javascripts/ku.js
@@ -1,134 +1,57 @@
-function gettime(){
-    var r=document.getElementById("ddd");
- var xhttp = new XMLHttpRequest();
-/* 4. Handle response (callback function) */
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-
-r.innerText="This page was last viewed "+this.responseText;
-console.log(this.responseText);
-
-}
-};
-/* 2. Open connection */
-xhttp.open("GET", "/last.txt", true);
-/* 3. Send request */
-xhttp.send();
-}
-
-
-
-
-function getcolor(){
-    var k=document.getElementById("lll");
- var xhttp = new XMLHttpRequest();
-/* 4. Handle response (callback function) */
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-
-k.innerHTML=this.responseText;
-k.style.color=this.responseText;
-
-}
-};
-/* 2. Open connection */
-xhttp.open("GET", "/color.txt", true);
-/* 3. Send request */
-xhttp.send();
-}
-
-
-function sisi(){
-    var oo=document.getElementById("oo");
-    var xhttp = new XMLHttpRequest();
-/* 4. Handle response (callback function) */
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-console.log(this.responseText);
-var qqq=JSON.parse(this.responseText);
-for (let index = 0; index < qqq.length; index++) {
-    oo.innerHTML+= "<li>"+qqq[index]+"</li>";
-
-}
-}
-};
-/* 2. Open connection */
-xhttp.open("GET", "/log.json", true);
-/* 3. Send request */
-xhttp.send();
-}
-
-setInterval(myGreeting, 10000);
-
-function myGreeting() {
-    var qwe=document.getElementById("oo");
-    var xhttp = new XMLHttpRequest();
-/* 4. Handle response (callback function) */
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-console.log(this.responseText);
-var rrr=JSON.parse(this.responseText);
-qwe.innerHTML="";
-for (let l = 0; l < rrr.length; l++) {
-    qwe.innerHTML+= "<li>"+rrr[l]+"</li>";
-
-}
-}
-};
-/* 2. Open connection */
-xhttp.open("GET", "/log-ro.json", true);
-/* 3. Send request */
-xhttp.send();
-
-}
-
-
-function zuihou(){
-    var xhttp = new XMLHttpRequest();
-/* 4. Handle response (callback function) */
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 403) {
-var e=document.getElementById("fff");
-e.innerHTML=`<p>ni bu xing a</p>
-<button type="button" onclick="button()">click me! </button>`;
-
-
-
-} else if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText);
-    var b=document.getElementById("fff");
-    b.innerHTML=this.responseText;
-
-
+function findex(){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4 &&xhr.status == 200)
+        {
+            document.getElementById("timer").innerText =`This page was last viewed ${xhr.response}`;
+        }
     }
-};
-/* 2. Open connection */
-xhttp.open("GET", "/content.ajax", true);
-/* 3. Send request */
-xhttp.send();
+    xhr.open("get","/last.txt");
+    xhr.send();
+}
 
 
 
 
-
+function sendRequest(){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState==4 && xhr.status==200)
+        {
+            let h1 =document.getElementById("heading");
+            h1.innerText =xhr.responseText;
+            h1.style.color =xhr.response;
+            // console.log(xhr.responseText);
+        }
+    }
+    xhr.open("get",'/color.txt');
+    xhr.send();
 
 }
 
-function button(){
-    var xhttp = new XMLHttpRequest();
-/* 4. Handle response (callback function) */
-xhttp.onreadystatechange = function() {
-zuihou();
 
-};
-/* 2. Open connection */
-xhttp.open("GET", "/accept", true);
-/* 3. Send request */
-xhttp.send();
+function flog2(){
+    let ul = document.getElementById("ul");
+    let xhr =new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4 && xhr.status == 200)
+        {
+            ul.innerHTML ="";
+            let res  =JSON.parse(xhr.response);
+            for (let i = 0; i < res.length; i++)
+            {
+                let li =document.createElement("li");
+                li.innerHTML = res[i];
+                ul.appendChild(li);
 
+            }
+        }
+    }
+    xhr.open('GET',"/log.json");
+    xhr.send();
 
-
-
-
-
+    setInterval(function(){
+        xhr.open("get","/log-ro.json")
+        xhr.send()
+    },10000)
 }
